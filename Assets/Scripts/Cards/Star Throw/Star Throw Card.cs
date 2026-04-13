afd02d2bb4b6793c705a2181fbad3e0272f588aa
pedrozampella2005 <pedrozampella@gmail.com>
2026-04-12 22:54:11 -0300

    IMPLEMENTACION DE LA CARTA STAR THROW

---
+++ Assets/Scripts/Cards/Star Throw/Star Throw Card.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarThrowCard : MonoBehaviour, ICardable
+{
+    [Header("Settings")]
+    [SerializeField] private GameObject sthPrefab;
+    [SerializeField] private float cooldownBetweenShots = 0.5f;
+    [SerializeField] private float reloadTime = 6f;
+    [SerializeField] private int maxShoots = 3;
+    private Image cardUI;
+    private int currentShoots;
+    private bool canShoot = true;
+
+
+
+    public void SetUI(Image uiImage)
+    {
+        cardUI = uiImage;
+    }
+
+
+    private void Awake()
+    {
+        currentShoots = maxShoots;
+    }
+
+
+    public void ExecuteCard(PlayerController player)
+    {
+        if (!canShoot || currentShoots <= 0) return;
+
+        canShoot = false;
+
+        GameObject fireball = Instantiate(sthPrefab, player.throwPoint.position, Quaternion.identity);
+
+        Vector2 direction = player.transform.localScale.x > 0 ? Vector2.right : Vector2.left;
+        if (fireball.TryGetComponent(out StarThrowProjectile script))
+        {
+            script.Init(direction, player.gameObject);
+        }
+
+        currentShoots--;
+
+        if (currentShoots > 0)
+        {
+            StartCoroutine(ShootCooldown());
+        }
+        else
+        {
+            StartCoroutine(ReloadCard());
+        }
+    }
+
+    private IEnumerator ShootCooldown()
+    {
+        yield return new WaitForSeconds(cooldownBetweenShots);
+        canShoot = true;
+    }
+
+    private IEnumerator ReloadCard()
+    {
+        if (cardUI != null) cardUI.enabled = false;
+
+        yield return new WaitForSeconds(reloadTime);
+        currentShoots = maxShoots;
+        canShoot = true;
+
+        if (cardUI != null) cardUI.enabled = true;
+    }
+}
