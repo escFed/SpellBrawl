b4e494950b10193aa0451b513266ab833c25c09d
leanuade <leandronicolasvf1@gmail.com>
2026-06-09 13:47:42 -0300

    asd

--- Assets/Scripts/StateMachine/Utility/ShieldState.cs
+++ Assets/Scripts/StateMachine/Utility/ShieldState.cs
@@ -28,14 +28,14 @@ public class ShieldState : PlayerState
             return;
         }
 
-        // Jump out of shield
+        
         if (character.JumpPressed)
         {
             stateMachine.ChangeState(StateCharacter.Jump);
             return;
         }
 
-        // Exit shield when J is released
+        
         if (!character.IsShieldHeld)
         {
             stateMachine.ChangeState(Mathf.Abs(character.MoveInput.x) > 0.01f
@@ -44,7 +44,7 @@ public class ShieldState : PlayerState
             return;
         }
 
-        // Dodge: A or D while shielding
+        
         if (Mathf.Abs(character.MoveInput.x) > 0.01f)
         {
             stateMachine.ChangeState(StateCharacter.Dodge);
